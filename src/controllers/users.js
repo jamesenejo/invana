2bc65f0f5526430958a5ef93ac49d75246710266
Enejo James Oche <james.enejo@andela.com>
2019-07-09 02:21:14 +0100

    users can login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,6 +11,10 @@ export default {
       firstname, lastname, email, password
     } = req.body;
 
+    // check for existence
+    const foundUser = Users.list.find(user => user.email === email);
+    if (foundUser) return res.jsend.fail('Email address already exists.');
+
     const user = {
       id: Users.list.length + 1,
       firstname,
@@ -24,6 +28,21 @@ export default {
 
     // sign jwt and wrap in a cookie
     const token = jwt.sign({ userId: user.id }, process.env.SECRET);
+    res.cookie('token', token, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    return res.jsend.success(token);
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    const foundUser = Users.list.find(user => user.email === email);
+    if (!foundUser) return res.jsend.fail('user does not exist.');
+    
+    const match = await bcrypt.compare(password, foundUser.password);
+    if (!match) return res.jsend.fail('Login failed!');
+
+    // sign jwt and wrap in a cookie
+    const token = jwt.sign({ userId: foundUser.id }, process.env.SECRET);
     res.cookie('token', token, { expires: new Date(Date.now() + 3600000), httpOnly: true });
 
     return res.jsend.success(token);
